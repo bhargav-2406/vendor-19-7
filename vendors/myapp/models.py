57645e029a1d1f528b098e6c85635b86e566d55d
bhargav-2406 <bhargavkrishnagali@gmail.com>
2024-07-22 09:41:51 +0530

    first commit

--- vendors/myapp/models.py
+++ vendors/myapp/models.py
@@ -5,7 +5,10 @@ class Product(models.Model):
     description = models.CharField(max_length=300)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     Business_name = models.CharField(max_length=255)
+    product_img = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
     
 
     def __str__(self):
         return self.product_name
+
+
